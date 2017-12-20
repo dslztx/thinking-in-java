12143d82e9cdcb4e50a9a2c18fe4938a82622ed2
dslztx <dslztx@gmail.com>
2017-12-20 23:41:35 +0800

    sync

---
+++ src/main/java/chapter6/ImportedMyClass.java
@@ -0,0 +1,11 @@
+package chapter6;
+
+import chapter6.MyClass;
+
+public class ImportedMyClass {
+
+  public static void main(String[] args) {
+    MyClass m = new MyClass();
+  }
+
+}
---
+++ src/main/java/chapter6/QualifiedMyClass.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class QualifiedMyClass {
+
+  public static void main(String[] args) {
+    chapter6.MyClass m = new chapter6.MyClass();
+  }
+
+}
