0bc48e6f44335e6ddbf87de7fcb2dc095f7fd1da
dslztx <dslztx@gmail.com>
2018-07-28 03:21:48 +0800

    sync

--- src/main/java/chapter21/ThreadExceptionUncaughtExceptionHandler.java
+++ src/main/java/chapter21/ThreadExceptionUncaughtExceptionHandler.java
@@ -19,7 +19,7 @@ public class ThreadExceptionUncaughtExceptionHandler {
               e.printStackTrace();
             }
 
-            System.out.println("can not run here");
+            System.out.println("can not run to here");
 
             try {
               Thread.sleep(10000);
